bc34be26f098365ee90a1d548d23a980f53bfd7f
Sujal1-hello <sujalrai395@gmail.com>
2026-05-05 00:18:36 +0545

    Made Small Cahnges On Language

--- calculation.py
+++ calculation.py
@@ -1,27 +1,25 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-print("Select operation:")
-op = input("Enter operation:")
+print("Select operation: +  -  *  /")
+op = input("Enter operation: ")
 
 if op == "+":
     result = num1 + num2
-    print("Result", result)
-    
+
 elif op == "-":
     result = num1 - num2
-    print("Result", result)
 
 elif op == "*":
     result = num1 * num2
-    print("Result", result)
 
 elif op == "/":
-    if num2 != 0:
-        result = num1 / num2
-        print("Result", result)
+    if num2 == 0:
+        result = "Cannot divide by zero"
     else:
-         print("Cannot divide by zero")
+        result = num1 / num2
 
 else:
-    print("invalid operation")
+    result = "Invalid operation"
+
+print("Result:", result)
