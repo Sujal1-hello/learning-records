99102cecf34c42b0cba7dd80576a61d69e1146bf
Sujal1-hello <sujalrai395@gmail.com>
2026-05-07 23:36:21 +0545

    Day 5: Java OOP class and object practice with structured folders

---
+++ Day5-Class-Object/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.name = "Ram";
+        s1.age = 20;
+
+        s1.display();
+    }
+}
