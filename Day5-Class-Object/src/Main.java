b060dd8c5fd3b9bf2e839bba63da2f5426a98336
Sujal1-hello <sujalrai395@gmail.com>
2026-05-07 23:39:35 +0545

    Enhanced Java OOP basics by adding comments and improving class-object implementation

--- Day5-Class-Object/src/Main.java
+++ Day5-Class-Object/src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
 
+        // Creating object of Student class
         Student s1 = new Student();
 
+        // Assigning values to object properties
         s1.name = "Ram";
         s1.age = 20;
 
+        // Calling method to display data
         s1.display();
     }
 }
--- Day5-Class-Object/src/Student.java
+++ Day5-Class-Object/src/Student.java
@@ -1,8 +1,12 @@
 public class Student {
+
+    // Instance variables (properties of Student)
     String name;
     int age;
 
+    // Method to display student details
     void display() {
+        System.out.println("---- Student Details ----");
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
     }
